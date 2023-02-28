1100a95146809e4bcf81f1f713b527c4f1de316a
nathancabigao <nathan0714@hotmail.com>
2023-02-28 15:01:42 -0500

    Implement #place_token, with tests

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,11 +2,30 @@
 
 # Instantiates a grid to be used for Connect Four games.
 class Grid
+  attr_reader :grid
+
   def initialize(grid = create_new_grid)
     @grid = grid # grid[col][row]
   end
 
   def create_new_grid
     Array.new(7) { Array.new(6) } # Grid of 7 columns with 6 rows
+  end
+
+  # Places token_char into the first available row in the given col if possible.
+  def place_token(token_char, col)
+    return 'Invalid column selection. Please enter the number of a column (1-7) that is not full.' if invalid_col(col)
+
+    row = @grid[col].count { |slot| !slot.nil? }
+    @grid[col][row] = token_char[0]
+  end
+
+  # Returns true if column is invalid (invalid column number, or column is full)
+  def invalid_col(col)
+    return true unless col.between?(0, 6)
+
+    return true if @grid[col].all? { |slot| !slot.nil? } # all rows are full
+
+    false
   end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -18,4 +18,51 @@ describe 'Grid' do
       expect(new_grid).to all(be_row_len_six)
     end
   end
+
+  describe '#place_token' do
+    context 'when the selected column is empty' do
+      subject(:grid_place_empty) { Grid.new }
+      it 'puts the token in the empty column, bottom row' do
+        token_char = "\u26AA"
+        col = 0
+        expect { grid_place_empty.place_token(token_char, col) }.to change { grid_place_empty.grid[col][0] }.from(nil).to("\u26AA")
+      end
+      it 'keeps only the first character if the token character was a string with 2+ characters' do
+        token_string = 'string'
+        col = 1
+        expect { grid_place_empty.place_token(token_string, col) }.to change { grid_place_empty.grid[col][0] }.from(nil).to('s')
+      end
+    end
+    context 'when the selected column has tokens, but is not full' do
+      grid_with_partial_column = Array.new(7) { Array.new(6) }
+      grid_with_partial_column[0] = ["\u26AA", "\u26AA", "\u26AA", nil, nil, nil]
+      subject(:grid_place_half) { Grid.new(grid_with_partial_column) }
+      it 'puts the token in the empty column, lowest available row' do
+        token_char = "\u26AA"
+        col = 0
+        expected_row = grid_place_half.grid[col].count { |slot| !slot.nil? }
+        expect { grid_place_half.place_token(token_char, col) }.to change { grid_place_half.grid[col][expected_row] }.from(nil).to("\u26AA")
+      end
+    end
+    context 'when the selected column is full' do
+      grid_with_full_column = Array.new(7) { Array.new(6) }
+      grid_with_full_column[0] = ["\u26AA", "\u26AA", "\u26AA", "\u26AB", "\u26AB", "\u26AB"]
+      subject(:grid_place_full) { Grid.new(grid_with_full_column) }
+      it 'returns an invalid column message' do
+        token_char = "\u26AA"
+        col = 0
+        invalid_col_message = 'Invalid column selection. Please enter the number of a column (1-7) that is not full.'
+        expect(grid_place_full.place_token(token_char, col)).to eq(invalid_col_message)
+      end
+    end
+    context 'when the given column number is invalid' do
+      subject(:grid_place_invalid) { Grid.new }
+      it 'returns an invalid column message' do
+        token_char = "\u26AA"
+        col = 8
+        invalid_col_message = 'Invalid column selection. Please enter the number of a column (1-7) that is not full.'
+        expect(grid_place_invalid.place_token(token_char, col)).to eq(invalid_col_message)
+      end
+    end
+  end
 end
